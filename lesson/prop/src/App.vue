1db701264c48dae39be5fdc82aab20e82916519f
Duncan Edwards <305185235@qq.com>
2021-01-25 21:15:24 +0800

    train prop

---
+++ lesson/prop/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <abc v-model='title' :site='obj' @parentChange='title=$event'/>
+    <button @click='obj.site="google"'>
+      change
+    </button>
+    {{title}}
+  </div>
+</template>
+
+<script>
+import Abc from "./components/Prop.vue"
+export default {
+  name: 'App',
+  components: {
+    Abc
+  },
+  data() {
+    return {
+      title:'google',
+      obj:{
+        name:'baidu.com'
+      }
+    }
+  },
+}
+</script>
+
---
+++ lesson/prop/src/components/Prop.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='hello'>
+    <input :value='value' @input="$emit('input',$event.target.value)">
+    {{value}}
+    <button @click='change'>
+      change
+    </button>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[
+    'value',
+    'site'
+  ],
+  methods:{
+    change() {
+      this.$emit('parentChange','tom')
+    }
+  }
+}
+</script>
+
